a7214c3f2ab4163356f3033cf71bad06163ee446
mckinled <douglas.e.mckinley@vanderbilt.edu>
2017-10-03 15:45:05 -0700

    Created youtube_student_projects.py, we're gonna scrape the video urls of past students capstone projects

--- web_scraping/youtube_student_projects.py
+++ web_scraping/youtube_student_projects.py
@@ -2,13 +2,14 @@ import requests
 from time import sleep
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
+import string
 import re
 from collections import Counter
 
 #we want to scrape all the Youtube links from past student capstone projects
 
 #step 1 let's put our base url somewhere convinient
-base_url = "http://eecs.oregonstate.edu/industry-relations/capstone-and-senior-design-projects"
+BASE_URL = "http://eecs.oregonstate.edu/industry-relations/capstone-and-senior-design-projects"
 
 #okay now time to grab a webpage
 def retrieve(url: str):
@@ -18,4 +19,28 @@ def retrieve(url: str):
     soup = BeautifulSoup(r.text, "lxml") #parse the HTML
     return soup
 
-print(retrieve(base_url)) #does retrieve work?
+#nice retrieve works let's get all the youtube links
+def get_links():
+    #we'll store the links here
+    result_list = {}
+
+    def load_video_links_from(url):
+        soup = retrieve(url)
+        #I'm a caveman so let's just grab every stinkin' link
+        links = soup.select("a ")
+
+        #but I only want youtube links
+        for link in links:
+            video_url = link.get('href')
+            if 'www.youtube.com' in video_url:
+                video_title = link.get('title')
+                print(video_title)
+                print(video_url)
+                result_list[video_title] = video_url
+                
+    load_video_links_from(BASE_URL)
+    return result_list
+
+#Did our link retrival work?
+links = get_links()
+print(links)
